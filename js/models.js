aa0c72e6ce691b98be1b68c6ee6b61bcd5c6e92a
Rucrisi <168000716+Rucrisi@users.noreply.github.com>
2025-03-22 18:19:31 +0100

    Add files via upload

--- js/models.js
+++ js/models.js
@@ -1,10 +1,22 @@
+const lang = getCurrentLang();
 // Cargar header
 fetch("models/header.html")
   .then(res => res.text())
   .then(data => {
     document.getElementById("header-placeholder").innerHTML = data;
+
     if (typeof setLanguage === "function") {
       setLanguage(lang);
+    }
+
+    // 🟢 Ahora sí existe el botón hamburguesa
+    const toggle = document.getElementById("hamburger");
+    const navMenu = document.getElementById("nav-menu");
+
+    if (toggle && navMenu) {
+      toggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+      });
     }
   });
 
@@ -14,6 +26,5 @@ fetch("models/footer.html")
   .then(data => {
     document.getElementById("footer-placeholder").innerHTML = data;
   });
-
 
 
